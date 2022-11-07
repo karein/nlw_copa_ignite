98654b5c9a9e61ecbbe73a6594ea0c390f63fb9d
karein <layla-poo@hotmail.com>
2022-11-07 11:31:51 -0300

    iniciando autenticação google, e criação de contextos; aula 03

---
+++ mobilee/src/screens/Signin.tsx
@@ -0,0 +1,33 @@
+
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons"
+
+import { useAuth } from '../hooks/useAuth'
+
+import Logo from '../assets/logo.svg';
+import { Button } from '../components/Button';
+
+export function SignIn() {
+  const { signIn, user } = useAuth()
+
+  console.log('DADOS DO USER', user)
+
+  return (
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40} />
+
+      <Button
+        mt={12}
+        type="SECONDARY"
+        onPress={signIn}
+        title="ENTRAR COM GOOGLE"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+      </Text>
+
+    </Center>
+  )
+}
